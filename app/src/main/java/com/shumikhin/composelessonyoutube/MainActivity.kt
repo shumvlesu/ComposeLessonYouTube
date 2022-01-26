fff35ccb96f048f71d2014fd85d22446a1ff2e47
shumikhin <sashumihin@gmail.com>
2022-01-26 14:02:52 +0300

    Initial commit

---
+++ app/src/main/java/com/shumikhin/composelessonyoutube/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.shumikhin.composelessonyoutube
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NewText()
+        }
+    }
+}
+
+@Preview (showBackground = true) // showBackground = true - показывать бэкграунд у виджета
+@Composable
+fun NewText(){
+    Text(text = "Hello 34")
+}
