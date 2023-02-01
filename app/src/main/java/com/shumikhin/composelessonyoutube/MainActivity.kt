0674a4b53c30071accc77e6f7db263cee2566909
shumihin.s <sashumihin@gmail.com>
2023-02-01 17:12:10 +0300

    Закончил 2й урок. Поиграл с контейнерами Column и Row и их вложенностью.

--- app/src/main/java/com/shumikhin/composelessonyoutube/MainActivity.kt
+++ app/src/main/java/com/shumikhin/composelessonyoutube/MainActivity.kt
@@ -3,23 +3,53 @@ package com.shumikhin.composelessonyoutube
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            NewText()
+            //Column (Столбец) - это один из видов контейнеров компоуза.
+//            Column() {
+//                Text(text = "Привет!")
+//                Text(text = "Краснодар!")
+//                Text(text = "Москва!")
+//            }
+            //Row() - это тоже контейнер.
+
+            Column( modifier = Modifier
+                .background(Color.Yellow)
+                .fillMaxSize(),//контейнер должен знаять весь экран
+                verticalArrangement = Arrangement.Bottom
+                ) {
+                Row(
+                    modifier = Modifier.background(Color.Red),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.Top
+                ) {
+                    Text(text = "Питер!", fontWeight = FontWeight.Bold, fontSize = 24.sp)
+                    Text(text = "Сочи!")
+                    Text("hello ".repeat(20), maxLines = 2)
+                }
+
+                Row(modifier = Modifier.background(Color.Green).fillMaxHeight(0.5f),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.Bottom
+                ) {
+                    Text(text = "Краснодар!", fontWeight = FontWeight.Bold, fontSize = 24.sp)
+                    Text(text = "Москва!")
+                    //Text("hello ".repeat(20), maxLines = 2)
+                }
+
+            }
         }
     }
-}
-
-@Preview(showBackground = true) // showBackground = true - показывать бэкграунд у виджета
-//Аннотация @Preview позволяет сразу видеть изменения в окне предпросмотра
-@Composable
-fun NewText() {
-    Text(text = "Привет!")
 }
